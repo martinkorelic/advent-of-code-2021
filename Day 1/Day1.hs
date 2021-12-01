aa5dfdc92e8671477884e5963232d9018e9683a3
martinkorelic <martin.korelic@protonmail.com>
2021-12-01 14:40:27 +0100

    Better day 1

--- Day 1/Day1.hs
+++ Day 1/Day1.hs
@@ -8,22 +8,20 @@ main = do
         let singlewords = words contents
             list = f singlewords
         -- Part 1 
-        writeFile "output_1" (show $ sonarSweep' list 0)
+        writeFile "output_1" (show $ sonarSweep' list)
         -- Part 2
-        writeFile "output_2" (show $ sonarSweep'' list [] 0)
+        writeFile "output_2" (show $ sonarSweep'' list [])
         hClose handle
         
 f :: [String] -> [Int]
 f = map read
 
-sonarSweep' :: [Int] -> Int -> Int
-sonarSweep' list c = sum $ zipWith (\x y -> fromEnum (x > y)) (drop 1 list) list
+sonarSweep' :: [Int] -> Int
+sonarSweep' list = sum $ zipWith (\x y -> fromEnum (x > y)) (drop 1 list) list
 
-sonarSweep'' :: [Int] -> [Int] -> Int -> Int
-sonarSweep'' [] sums c = sonarSweep' sums c 
-sonarSweep'' [_] sums c = sonarSweep' sums c
-sonarSweep'' [_,_] sums c = sonarSweep' sums c
-sonarSweep'' (y1:y2:y3:ys) sums c = sonarSweep'' (y2:y3:ys) (sums ++ [y1+y2+y3]) c
+sonarSweep'' :: [Int] -> [Int] -> Int
+sonarSweep'' (y1:y2:y3:ys) sums = sonarSweep'' (y2:y3:ys) (sums ++ [y1+y2+y3])
+sonarSweep'' _ sums = sonarSweep' sums
 
 -- Extra
 sonarSweepZ :: [Int] -> Int -> Int
